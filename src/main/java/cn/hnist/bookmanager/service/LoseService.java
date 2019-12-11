a0084f2b840d78dab4bc3ae15ca1e16775bf3e5c
weimo001 <48851999+weimo001@users.noreply.github.com>
2019-12-11 15:32:36 +0800

    微墨写的lose类，包括及测试方法

---
+++ src/main/java/cn/hnist/bookmanager/service/LoseService.java
@@ -0,0 +1,48 @@
+package cn.hnist.bookmanager.service;
+
+import cn.hnist.bookmanager.model.Lose;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * 遗失情况Service,里面包含了遗失信息的操作
+ * @author 向清润
+ * @createTime 2019.12.10.19:38
+ */
+public interface LoseService {
+
+    /**
+     * 管理员添加遗失信息
+     * @param lose 遗失信息实体
+     * @return 受影响行数
+     */
+    int addLose(Lose lose);
+
+    /**
+     * 根据用户Id查询遗失信息
+     * @param page 当前页
+     * @param pageSize 页面大小
+     * @param userId 用户Id
+     * @return 该用户所有遗失信息的列表
+     */
+    PageInfo<Lose> searchLoseByUserId(int page, int pageSize, int userId);
+
+    /**
+     * 根据遗失信息Id查询遗失信息
+     * @param loseId 遗失信息Id
+     * @return 遗失信息实体
+     */
+    Lose searchLoseByLoseId(int loseId);
+
+
+    /**
+     * 查询
+     * @param page 当前页
+     * @param pageSize 页面大小
+     * @return
+     */
+    PageInfo<Lose> searchLose(int page, int pageSize);
+
+
+}
