b4886fb0057ffbd9ffc2b605f81fbd2681aa2413
tzf-word <2625066139@qq.com>
2019-12-11 17:31:10 +0800

    tzf bookservice以及bookservice测试类的提交

--- src/main/java/cn/hnist/bookmanager/service/impl/BookServiceImpl.java
+++ src/main/java/cn/hnist/bookmanager/service/impl/BookServiceImpl.java
@@ -3,10 +3,14 @@ package cn.hnist.bookmanager.service.impl;
 import cn.hnist.bookmanager.mapper.BookMapper;
 import cn.hnist.bookmanager.model.Book;
 import cn.hnist.bookmanager.model.BookExample;
+import cn.hnist.bookmanager.model.Booktype;
 import cn.hnist.bookmanager.service.BookService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -17,6 +21,7 @@ import java.util.List;
 public class BookServiceImpl implements BookService {
 
     @Autowired
+    @Resource
     private BookMapper bookMapper;
 
     /**
@@ -34,5 +39,171 @@ public class BookServiceImpl implements BookService {
             return null;
         }
         return books.get(0);
+    }
+
+    /**
+     * 增加图书
+     * @param book
+     * @return
+     */
+    @Override
+    public int addBook(Book book) {
+        if(book != null && book.getBookName() != null){
+            return bookMapper.insert(book);
+        }
+        return 0;
+    }
+
+    /**
+     * 根据图书id删除图书
+     * @param id
+     * @return
+     */
+    @Override
+    public int deleteBook(int id) {
+        return bookMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 根据图书id修改图书信息
+     * @param id
+     * @param book
+     * @return
+     */
+    @Override
+    public int updateBook(int id, Book book) {
+        if(book != null && id ==book.getBookId()){
+            BookExample bookExample = new BookExample();
+            bookExample.or().andBookIdEqualTo(id);
+            return bookMapper.updateByExample(book,bookExample);
+        }
+        return 0;
+    }
+
+    /**
+     * 通过图书名查询图书id
+     * @param name
+     * @return
+     */
+    @Override
+    public int selectBookIdByName(String name) {
+        BookExample bookExample = new BookExample();
+        bookExample.or().andBookNameEqualTo(name);
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        if (books != null && books.size() != 0){
+            return books.get(0).getBookId();
+        }
+        return 0;
+    }
+
+    /**
+     * 通过书名查询图书
+     * @param name
+     * @return
+     */
+    @Override
+    public Book selectBookByName(String name) {
+        BookExample bookExample = new BookExample();
+        bookExample.or().andBookNameEqualTo(name);
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        if(books != null && books.size()>0){
+            return books.get(0);
+        }
+        return null;
+    }
+
+    /**
+     * 通过图书类型查询图书
+     * @param booktype
+     * @return
+     */
+    @Override
+    public List<Book> selectBookByType(Booktype booktype) {
+        BookExample bookExample = new BookExample();
+        bookExample.or().andBookTypeEqualTo(booktype.getTypeId());
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        if(books != null){
+            return books;
+        }
+        return null;
+    }
+
+    /**
+     * 通过作者名查询图书
+     * @param authorname
+     * @return
+     */
+    @Override
+    public List<Book> selectBookByAuthor(String authorname) {
+        BookExample bookExample = new BookExample();
+        bookExample.or().andAuthorEqualTo(authorname);
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        if(books != null){
+            return books;
+        }
+        return null;
+    }
+
+    /**
+     * 通过关键字查询图书
+     * @param keyword
+     * @return
+     */
+    @Override
+    public List<Book> selectBookByKeyWord(String keyword) {
+        BookExample bookExample = new BookExample();
+        bookExample.or().andBookNameLike("%"+keyword+"%");
+        bookExample.or().andAuthorLike("%"+keyword+"%");
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        if(books != null){
+            return books;
+        }
+        return null;
+    }
+
+    /**
+     * 通过标签查询图书
+     * @param labelName
+     * @return
+     */
+    @Override
+    public List<Book> selectBookByLabelName(String labelName) {
+        List<Book> books = bookMapper.selctByLabelName(labelName);
+        if(books != null){
+            return books;
+        }
+        return null;
+    }
+
+    /**
+     * 查询所有图书
+     * @return
+     */
+    @Override
+    public List<Book> selectAllBook() {
+        BookExample bookExample = new BookExample();
+        bookExample.or();
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        if(books != null){
+            return books;
+        }
+        return null;
+    }
+
+    /**
+     * 通过page查询图书
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @Override
+    public PageInfo queryBookByPage(int page, int pageSize) {
+        PageHelper.startPage(page,pageSize);
+        BookExample bookExample = new BookExample();
+        bookExample.or();
+        List<Book> books = bookMapper.selectByExample(bookExample);
+        PageInfo pageInfo = new PageInfo(books,5);
+        return pageInfo;
+
     }
 }
--- src/test/java/cn/hnist/bookmanager/service/impl/BookServiceImplTest.java
+++ src/test/java/cn/hnist/bookmanager/service/impl/BookServiceImplTest.java
@@ -1,6 +1,8 @@
 package cn.hnist.bookmanager.service.impl;
 
 import cn.hnist.bookmanager.BookManagerApplication;
+import cn.hnist.bookmanager.model.Book;
+import cn.hnist.bookmanager.model.Booktype;
 import cn.hnist.bookmanager.service.BookService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -8,12 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @author whg
- * @date 2019/12/9 20:49
- **/
+ * @Author tzf
+ * @Date 2019/12/11 15:43
+ * @Version 1.0
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookManagerApplication.class)
 class BookServiceImplTest {
@@ -24,5 +32,93 @@ class BookServiceImplTest {
     @Test
     void findBookById() {
         System.out.println(bookService.findBookById(1));
+    }
+
+    @Test
+    void addBook() {
+        Book book = new Book();
+        book.setBookName("三国演义");
+        book.setAuthor("罗贯中");
+        book.setBookPicture("xxx");
+        book.setBookType(1);
+        book.setIntroduce("四大名著-三国演义");
+        book.setPrice(20.0);
+        book.setBookCount(100);
+        book.setBookNum(100);
+        book.setBookshelf(1);
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String time = sdf.format(date);
+        book.setBookIsbn(time);
+        System.out.println(bookService.addBook(book));
+    }
+
+    @Test
+    void deleteBook() {
+        System.out.println(bookService.deleteBook(1));
+    }
+
+    @Test
+    void updateBook() {
+        Book book = new Book();
+        book.setBookId(2);
+        book.setBookName("三国演义");
+        book.setAuthor("罗贯中");
+        book.setBookPicture("xxx");
+        book.setBookType(1);
+        book.setIntroduce("四大名著-三国演义");
+        book.setPrice(80.0);
+        book.setBookCount(100);
+        book.setBookNum(100);
+        book.setBookshelf(1);
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String time = sdf.format(date);
+        book.setBookIsbn(time);
+        System.out.println(bookService.updateBook(book.getBookId(),book));
+    }
+
+    @Test
+    void selectBookIdByName() {
+        System.out.println(bookService.selectBookIdByName("三国演义"));
+    }
+
+    @Test
+    void selectBookByName() {
+        System.out.println(bookService.selectBookByName("三国演义"));
+    }
+
+    @Test
+    void selectBookByType() {
+        Booktype booktype = new Booktype();
+        booktype.setTypeId(1);
+        booktype.setTypeName("小说");
+        System.out.println(bookService.selectBookByType(booktype));
+    }
+
+    @Test
+    void selectBookByAuthor() {
+        System.out.println(bookService.selectBookByAuthor("罗贯中"));
+    }
+
+    @Test
+    void selectBookByKeyWord() {
+        System.out.println(bookService.selectBookByKeyWord("罗"));
+        System.out.println(bookService.selectBookByKeyWord("三"));
+    }
+
+    @Test
+    void selectBookByLabelName() {
+        System.out.println(bookService.selectBookByLabelName("小说"));
+    }
+
+    @Test
+    void selectAllBook() {
+        System.out.println(bookService.selectAllBook());
+    }
+
+    @Test
+    void queryBookByPage() {
+        System.out.println(bookService.queryBookByPage(1,2).getList().size());
     }
 }
