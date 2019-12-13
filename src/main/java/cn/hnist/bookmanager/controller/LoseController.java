b62b4876e04882dc5b93d15ae313541398d2302d
weimo001 <48851999+weimo001@users.noreply.github.com>
2019-12-13 11:47:43 +0800

    lk写的lose类，controller层

---
+++ src/main/java/cn/hnist/bookmanager/controller/LoseController.java
@@ -0,0 +1,28 @@
+package cn.hnist.bookmanager.controller;
+
+import cn.hnist.bookmanager.service.impl.LoseServiceImpl;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * @author 向清润
+ * @createTime 2019.12.13.10:51
+ */
+@Controller
+public class LoseController {
+    @Autowired
+    LoseServiceImpl loseService;
+    @RequestMapping("/loseList")
+    public ModelAndView loseList(@RequestParam(value = "page",defaultValue = "1") int page){
+        ModelAndView modelAndView = new ModelAndView("/admin/loseList");
+        PageInfo<Map> pageInfo = loseService.searchLose(page, 10);
+        modelAndView.addObject("pageInfo",pageInfo);
+        return modelAndView;
+    }
+}
